36d9dc6d59a23be6e3f0bd3a28e94831ede54fc5
sujanprajapati <sujan.prajapati6539@gmail.com>
2024-07-04 18:51:51 +0545

    clean up.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(project(":uxPulse"))
-    implementation(files("/Users/sujanprajapati/Downloads/adobeMobileLibrary-4.18.2.jar"))
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
@@ -65,8 +64,4 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.google.firebase:firebase-analytics:22.0.2")
 
-    implementation(platform("com.adobe.marketing.mobile:sdk-bom:3.+"))
-    implementation("com.adobe.marketing.mobile:core")
-    implementation("com.adobe.marketing.mobile:identity")
-    implementation("com.adobe.marketing.mobile:analytics")
 }
